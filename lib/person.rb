88f93dd0a2bf25b39589e4add3ca31e47b70003c
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-06-26 08:20:37 -0400

    commit tree ff27e08b3da2b7fbc22d55e61355058cb9ac0f1b

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,12 @@
 # your code goes here
+require 'pry'
+
+class Person 
+    attr_reader :name
+    attr_accessor :bank_account, :happiness,:hygiene
+    def initialize(bank_account, happiness, hygiene)
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+end
